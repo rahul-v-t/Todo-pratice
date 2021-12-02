a71a0f34fb0c72944b01f2c9214b0c4cac0acad2
rahulthambi10@gmail.com <rahulthambi10@gmail.com>
2021-12-02 11:53:23 +0530

    Updated

--- src/components/screens/Todo.js
+++ src/components/screens/Todo.js
@@ -4,10 +4,13 @@ import Add from "../assets/plus.svg";
 import Tick from "../assets/tick-green.svg";
 import Revert from "../assets/revert.svg";
 import styled from "styled-components";
-import { Button } from 'antd';
+import { Layout, Menu, Breadcrumb,Button } from 'antd';
+import { UserOutlined, LaptopOutlined, NotificationOutlined } from '@ant-design/icons';
 import 'antd/dist/antd.css';
 
 export default function Todo() {
+    const { SubMenu } = Menu;
+    const { Header, Content, Sider } = Layout;
     const[items, setItems] = useState([]);
     const [input, setInput] = useState("");
     const [newId,setNewId] = useState(1)
@@ -42,7 +45,58 @@ export default function Todo() {
     };
     return (
         <>
-        <Box>
+        <Layout>
+    <Header className="header">
+      <div className="logo" />
+      <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']}>
+        <Menu.Item key="1">nav 1</Menu.Item>
+        <Menu.Item key="2">nav 2</Menu.Item>
+        <Menu.Item key="3">nav 3</Menu.Item>
+      </Menu>
+    </Header>
+    <Layout>
+      <Sider width={200} className="site-layout-background">
+        <Menu
+          mode="inline"
+          defaultSelectedKeys={['1']}
+          defaultOpenKeys={['sub1']}
+          style={{ height: '100%', borderRight: 0 }}
+        >
+          <SubMenu key="sub1" icon={<UserOutlined />} title="subnav 1">
+            <Menu.Item key="1">option1</Menu.Item>
+            <Menu.Item key="2">option2</Menu.Item>
+            <Menu.Item key="3">option3</Menu.Item>
+            <Menu.Item key="4">option4</Menu.Item>
+          </SubMenu>
+          <SubMenu key="sub2" icon={<LaptopOutlined />} title="subnav 2">
+            <Menu.Item key="5">option5</Menu.Item>
+            <Menu.Item key="6">option6</Menu.Item>
+            <Menu.Item key="7">option7</Menu.Item>
+            <Menu.Item key="8">option8</Menu.Item>
+          </SubMenu>
+          <SubMenu key="sub3" icon={<NotificationOutlined />} title="subnav 3">
+            <Menu.Item key="9">option9</Menu.Item>
+            <Menu.Item key="10">option10</Menu.Item>
+            <Menu.Item key="11">option11</Menu.Item>
+            <Menu.Item key="12">option12</Menu.Item>
+          </SubMenu>
+        </Menu>
+      </Sider>
+      <Layout style={{ padding: '0 24px 24px' }}>
+        <Breadcrumb style={{ margin: '16px 0' }}>
+          <Breadcrumb.Item>Home</Breadcrumb.Item>
+          <Breadcrumb.Item>List</Breadcrumb.Item>
+          <Breadcrumb.Item>App</Breadcrumb.Item>
+        </Breadcrumb>
+        <Content
+          className="site-layout-background"
+          style={{
+            padding: 24,
+            margin: 0,
+            minHeight: 280,
+          }}
+        >
+          <Box>
             <Heading>Todo List</Heading>
             <Head>Things To Be Done</Head>
             <List>
@@ -55,7 +109,7 @@ export default function Todo() {
                 <Form>
                     <Img src={Add} />
                     <Input value={input} onChange={(e)=>setInput(e.target.value)} placeholder="Add New Task" />
-                    <But onClick={addItems} >Add New</But>
+                    <Button type="primary" onClick={addItems}>Add New</Button>
                 </Form>
             </Items>
             <Head>Completed</Head>
@@ -65,8 +119,13 @@ export default function Todo() {
                                 removeItem(completed.id);}} /> <img src={Delete} onClick={() => removeItem(completed.id)} /></div> </Elements>
                 ))}
             </List>
-            <Button type="primary">Primary Button</Button>
+            
         </Box>
+        </Content>
+      </Layout>
+    </Layout>
+  </Layout>
+        
         </>
     )
 }
